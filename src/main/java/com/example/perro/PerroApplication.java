cd527685bf31ad29a7d34c4480e6c0decc9007ee
Víctor Ferré <victor522m@gmail.com>
2025-03-21 14:16:23 +0100

    "First Commit"

---
+++ src/main/java/com/example/perro/PerroApplication.java
@@ -0,0 +1,13 @@
+package com.example.perro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/perro/PerroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
